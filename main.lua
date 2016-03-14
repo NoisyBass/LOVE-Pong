1a291131bc82408967df421d741644c5cbb7491d
NoisyBass <laura.dcs4@gmail.com>
2016-03-14 20:46:56 +0100

    Player paddle move

--- main.lua
+++ main.lua
@@ -11,6 +11,7 @@ function love.load()
 
 	love.graphics.setBackgroundColor(21, 24, 24)
 	love.window.setMode(conf.window.width, conf.window.height)
+	love.graphics.setFont(love.graphics.newFont(60))
 
 	-- world setup
 	world = love.physics.newWorld(0, 0, false)
@@ -44,16 +45,26 @@ function love.load()
 end
 
 function love.update(dt)
+	mouseControl = true
+	keyboardControl = false
+	local y = objects.player.body: getY()
 
+	if mouseControl then
+		y = love.mouse.getY()
+	elseif keyboardControl then
+		if love.keyboard.isDown("up") then
+			y = y - 200 * dt
+		elseif love.keyboard.isDown("down") then
+			y = y + 200 * dt
+		end
+	end
+
+	if y > conf.paddle.height/2 and y < conf.window.height - conf.paddle.height/2 then
+		objects.player.body: setY(y)
+	end
 end
 
 function love.draw()
-	love.graphics.setColor(92, 115, 114)
-	-- player score
-	love.graphics.printf(objects.player.score, conf.window.width/2 - 60, 20, 40, "right")
-	-- opponent score
-	love.graphics.printf(objects.opponent.score, conf.window.width/2 + 20, 20, 40, "left")
-
 	-- player paddle
 	love.graphics.setColor(36, 163, 154)
 	love.graphics.polygon("fill", objects.player.body: getWorldPoints(objects.player.shape: getPoints()))
@@ -62,12 +73,18 @@ function love.draw()
 	love.graphics.setColor(255, 143, 57)
 	love.graphics.polygon("fill", objects.opponent.body: getWorldPoints(objects.opponent.shape: getPoints()))
 
-	-- ball
 	love.graphics.setColor(86, 93, 93)
+	-- ball
 	love.graphics.circle("fill", objects.ball.body: getX(), objects.ball.body: getY(), objects.ball.shape: getRadius())
 
 	-- draw line
 	for i = 0, conf.window.height/50 - 1 do
 		love.graphics.rectangle("fill", conf.window.width/2 - 5, i*50, 10, 40)
 	end
+
+	love.graphics.setColor(92, 115, 114)
+	-- player score
+	love.graphics.printf(objects.player.score, conf.window.width/2 - 80, 20, 40, "right")
+	-- opponent score
+	love.graphics.printf(objects.opponent.score, conf.window.width/2 + 40, 20, 40, "left")
 end
