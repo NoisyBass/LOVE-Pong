61d8c6fffa55b6a9e6532878d4d4d9fd9a7f8e73
NoisyBass <laura.dcs4@gmail.com>
2016-03-16 19:33:01 +0100

    Configuration added

---
+++ conf.lua
@@ -0,0 +1,25 @@
+function love.conf(t)
+    t.version = "0.10.1"                -- The LÖVE version this game was made for (string)
+    t.console = true                    -- Attach a console (boolean, Windows only)
+ 
+    t.window.title = "Pong"         -- The window title (string)
+    t.window.width = 800                -- The window width (number)
+    t.window.height = 600               -- The window height (number)
+ 
+    t.modules.audio = true              -- Enable the audio module (boolean)
+    t.modules.event = true              -- Enable the event module (boolean)
+    t.modules.graphics = true           -- Enable the graphics module (boolean)
+    t.modules.image = true              -- Enable the image module (boolean)
+    t.modules.joystick = false           -- Enable the joystick module (boolean)
+    t.modules.keyboard = true           -- Enable the keyboard module (boolean)
+    t.modules.math = true               -- Enable the math module (boolean)
+    t.modules.mouse = true              -- Enable the mouse module (boolean)
+    t.modules.physics = true            -- Enable the physics module (boolean)
+    t.modules.sound = true              -- Enable the sound module (boolean)
+    t.modules.system = true             -- Enable the system module (boolean)
+    t.modules.timer = true              -- Enable the timer module (boolean), Disabling it will result 0 delta time in love.update
+    t.modules.touch = false              -- Enable the touch module (boolean)
+    t.modules.video = false              -- Enable the video module (boolean)
+    t.modules.window = true             -- Enable the window module (boolean)
+    t.modules.thread = false             -- Enable the thread module (boolean)
+end
